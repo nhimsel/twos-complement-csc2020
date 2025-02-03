fbabca17597403440f6df1a90dc235fa06e1a541
nhimsel <nathanael.himsel@cuw.edu>
2025-02-03 17:15:27 -0600

    fix for negative ints

--- two's-complement.c
+++ two's-complement.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
-#include <string.h>
 
 void twosComplement(int n, char* r);
 
 void intToBinary(int n, char* str);
 
-void addBinary(char* str, int n, char* r);
+void addBinary(char* str, int n);
 
-void complement(char* str, char* r);
+void invertBinary(char* str);
 
 char itoc(int n);
 
@@ -15,76 +14,94 @@ int ctoi(char c);
 
 int main()
 {
-    char r[32];
+    char str[32]="";
     int num;
 
     printf("Enter an int: ");
     scanf("%d", &num);
 
-    twosComplement(num,r);
+    printf("%s", "You entered: ");
+    printf("%d\n", num);
 
-    printf("%s\n", r);
+    twosComplement(num,str);
+
+    printf("%s", "Two's Complement: ");
+    printf("%s\n", str);
 }
 
-void twosComplement(int n, char* r)
+void twosComplement(int n, char* str)
 {
-    char str[32];
-
     intToBinary(n, str);
 
-    complement(str, r);
+    printf("%s", "In Binary: ");
+    printf("%s\n", str);
+
+    invertBinary(str);
+
+    addBinary(str, 1);
 }
 
 void intToBinary(int n, char* str)
 {
+    int origN=n;
+
+    if (n<0) //itoc breaks if n<1
+    {
+        n*=-1;
+    }
+
     for (int i=31; i>=0; i--)
     {
         str[i]=(itoc(n%2));
         n/=2;
     }
+
+    if (origN<0) //fix negative values
+    {
+        invertBinary(str);
+        addBinary(str, 1);
+    }
 }
 
-void addBinary(char* str, int n, char* r)
+void addBinary(char* str, int n)
 {
     char str2[32];
     intToBinary(n, str2);
 
-    int c=0; 
+    int c=0; //hold carry value for addition
     for(int i=31; i>=0; i--)
     {
-        int temp=(ctoi(str[i]))+(ctoi(str2[i]))+c; //conver the values from char to int, then add
+        int temp=(ctoi(str[i]))+(ctoi(str2[i]))+c;
         if (temp>=2)
         {
-            r[i]=itoc(temp-2);
+            str[i]=itoc(temp-2);
             c=1;
         }
         else
         {
-            r[i]=itoc(temp);
+            str[i]=itoc(temp);
             c=0;
         }
     }
-
 }
 
-void complement(char* str, char* r)
+void invertBinary(char* str)
 {
     for (int i=31; i>=0; i--)
     {
         if(str[i]=='0')
         {
-            r[i]='1';
+            str[i]='1';
         }
         else if (str[i]=='1')
         {
-            r[i]='0';
+            str[i]='0';
         }
-        else
+        else //should never happen
         {
             printf("%s\n", "error. binary value holds a value that is not 0 or 1");
         }
     }
-    addBinary(r, 1, r);
 }
 
 char itoc(int n)
